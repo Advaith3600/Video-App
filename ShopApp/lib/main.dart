393d01cb9772215ee4a0a5ce4db7af97d88b48e5
hkk710 <hkk710@gmail.com>
2020-10-15 21:04:59 +0530

    Shop app added

---
+++ ShopApp/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:ShopApp/models/product.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_details_screen.dart';
+import './providers/products.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+        },
+      ),
+    );
+  }
+}
+
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('MyShop'),
+//       ),
+//       body: Center(
+//         child: Text('Let\'s build a shop'),
+//       ),
+//     );
+//   }
+// }
